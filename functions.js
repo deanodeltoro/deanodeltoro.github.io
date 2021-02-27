a5366ee04cd726452a285e349c8eb55acae38762
q <crlarrea@outlook.com>
2021-02-27 17:37:08 +0000

    updated

--- functions.js
+++ functions.js
@@ -29,29 +29,15 @@ function hideHike(n) {
 }
 
 
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
 var slideId = ["gambuesaSlideshow", "pintoSlideshow",
 "tirajanaSlideshow","berrielSlideshow","Ayagaures2TunteSlideshow",
 "BcodelToroSlideshow","llanosCircularoSlideshow",
 "guayadequeSlideshow","oscuroSlideshow","ventanaNubloSlideshow",
 "hornosSlideshow","nievesSlideshow", "hondoSlideshow",
-"azuajeVirgenSlideshow"]
+"azuajeVirgenSlideshow","charcoAzulSlideshow","teriscalSlideshow"]
 
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-showSlides(1, 4);
-showSlides(1, 5);
-showSlides(1, 6);
-showSlides(1, 7);
-showSlides(1, 8);
-showSlides(1, 9);
-showSlides(1, 10);
-showSlides(1, 11);
-showSlides(1, 12);
-showSlides(1, 13);
 
 
 function plusSlides(n, no) {
