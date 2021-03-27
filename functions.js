7586b80575c2d87f2c1749b1441feed941abfbfd
q <crlarrea@outlook.com>
2021-03-27 18:17:53 +0000

    updated website

--- functions.js
+++ functions.js
@@ -3,8 +3,8 @@ function toggleInfo(){
 	let y = x.previousElementSibling;
 
 	// alert(x.height);
-	if (y.style.height != "150px") {
-		y.style.height = "150px";
+	if (y.style.height != "400px") {
+		y.style.height = "400px";
 		y.style.overflowY = "scroll";
 		x.innerHTML = "&#61544;";
 	} 
@@ -18,12 +18,26 @@ function toggleInfo(){
 
 function myFunction() {
 
-	var area = document.getElementById("areas").value.replace(/\s/g,"");
+	var area = document.getElementById("areas").value;
+
+	if (area == "all") {
+		for (let el of document.querySelectorAll('.hikeContainer')) 
+		el.style.display = 'flex';
+	}
 	
+	else {
 	for (let el of document.querySelectorAll('.hikeContainer')) 
 	el.style.display = 'none';
 
-	for (let el of document.querySelectorAll('.hikeContainer' + "."+ area)) 
+	for (let el of document.querySelectorAll('.municipalityClass')) 
+	el.style.display = 'none';
+
+	for (let el of document.querySelectorAll('.hikeContainer' + "."+ area))
 	el.style.display = 'flex';
+	
+
+	for (let el of document.querySelectorAll('.municipalityClass' + "."+ area))
+	el.style.display = 'flex';
+	}
 }
 
