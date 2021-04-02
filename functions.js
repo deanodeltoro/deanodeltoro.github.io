a8c4ce879aadad2fd3256d4d7d434826971c4048
q <crlarrea@outlook.com>
2021-04-02 11:08:02 +0100

    revised code

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function toggleInfo(){
+function toggleInfo() {
 	let x = document.activeElement;
 	let y = x.previousElementSibling;
 
@@ -7,8 +7,8 @@ function toggleInfo(){
 		y.style.height = "400px";
 		y.style.overflowY = "scroll";
 		x.innerHTML = "&#61544;";
-	} 
-	else { 
+	}
+	else {
 		y.style.height = "0px";
 		x.innerHTML = "&#61543;";
 	}
@@ -20,29 +20,35 @@ function myFunction() {
 
 	var area = document.getElementById("areas").value;
 
-	if (area == "all") {
-		for (let el of document.querySelectorAll('.municipalityClass')) 
+	for (let el of document.querySelectorAll('.municipalityClass')) {
 		el.style.display = 'none';
-
-		for (let el of document.querySelectorAll('.hikeContainer')) 
-		el.style.display = 'flex';
-
-		for (let el of document.querySelectorAll('.municipalityClass' + ".granCanaria" )) 
-		el.style.display = 'flex';
 	}
-	
+
+	for (let el of document.querySelectorAll('.hikeContainer')) {
+		el.style.display = 'none';
+	}
+
+	if (area == "all") {
+
+
+		for (let el of document.querySelectorAll('.hikeContainer')) {
+			el.style.display = 'flex';
+		}
+
+		for (let el of document.querySelectorAll('.municipalityClass' + ".granCanaria")) {
+			el.style.display = 'flex';
+		}
+	}
+
 	else {
-		for (let el of document.querySelectorAll('.hikeContainer')) 
-		el.style.display = 'none';
 
-		for (let el of document.querySelectorAll('.municipalityClass')) 
-		el.style.display = 'none';
+		for (let el of document.querySelectorAll('.hikeContainer' + "." + area)) {
+			el.style.display = 'flex';
+		}
 
-		for (let el of document.querySelectorAll('.hikeContainer' + "."+ area))
-		el.style.display = 'flex';
-	
-		for (let el of document.querySelectorAll('.municipalityClass' + "."+ area))
-		el.style.display = 'flex';
+		for (let el of document.querySelectorAll('.municipalityClass' + "." + area)) {
+			el.style.display = 'flex';
+		}
 	}
 }
 
